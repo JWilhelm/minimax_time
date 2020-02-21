904d24684d102ab14a47c500020813307fad8161
Jan Wilhelm <wij17778@pc1011600161.uni-regensburg.de>
2020-02-21 22:48:23 +0100

    get all minima and maxima of the error function

--- minimax_time.py
+++ minimax_time.py
@@ -21,28 +21,33 @@ def main():
 
     alphas_betas_init = np.logspace(-3,-2,2*n_minimax)
 
-    print("alphas_betas_init =", alphas_betas_init)
-    print("shape alphas_betas_init =", np.shape(alphas_betas_init))
-
     alphas_betas_L2_opt, alphas_betas_conv = curve_fit(eta, xdata, ydata, p0=alphas_betas_init)
 
     i = 0
-    while i < 10:
+    while i < 1:
 
-#        maxm = argrelextrema(y, np.greater)
-#        minm = argrelextrema(y, np.less)
+        max_x = xdata[argrelextrema(eta_plotting(xdata,alphas_betas_L2_opt), np.greater)[0]]
+        min_x = xdata[argrelextrema(eta_plotting(xdata,alphas_betas_L2_opt), np.less)[0]]
+
+        if eta_plotting(xdata[0],alphas_betas_L2_opt) > 0:
+            max_x = np.append(xdata[0], max_x)
+        else:
+            min_x = np.append(xdata[0], min_x)
+
         i += 1
 
 
-
-    print("alphas_betas_L2_opt =", alphas_betas_L2_opt)
-    print("shape alphas_betas_L2_opt =", np.shape(alphas_betas_L2_opt))
+    print ("maxm =", max_x)
+    print ("eta_plotting(max_x,alphas_betas_L2_opt) =", eta_plotting(max_x,alphas_betas_L2_opt))
+    print ("minm =", min_x)
+    print ("eta_plotting(min_x,alphas_betas_L2_opt) =", eta_plotting(min_x,alphas_betas_L2_opt))
 
 
     fig1, (axis1) = pl.subplots(1,1)
     axis1.set_xlim((1,R_minimax))
-    axis1.set_ylim((10e-15,1))
-    axis1.loglog(xdata,np.abs(eta_plotting(xdata,alphas_betas_L2_opt)))
+    axis1.semilogx(xdata,eta_plotting(xdata,alphas_betas_L2_opt))
+    axis1.semilogx(max_x, eta_plotting(max_x,alphas_betas_L2_opt), "x")
+    axis1.semilogx(min_x, eta_plotting(min_x,alphas_betas_L2_opt), "x")
 
     pl.show()
 

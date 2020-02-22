c0b77adf141d19b05a40e68d562272d5264acfd4
Jan Wilhelm <wij17778@pc1011600161.uni-regensburg.de>
2020-02-22 22:22:13 +0100

    small test script for minimax

--- check_minimax.py
+++ check_minimax.py
@@ -19,30 +19,11 @@ def main():
     alphas_betas = np.loadtxt("alpha_beta_of_N_"+str(n_minimax))
     alphas_betas_E = np.append(alphas_betas,1)
 
-    E_old = alphas_betas_E[-1]*2
-
-    sort_indices = np.argsort(alphas_betas_E[0:n_minimax])
-    np.savetxt("alpha_beta_of_N_"+str(n_minimax)+"_L2", np.append(alphas_betas_E[sort_indices],alphas_betas_E[sort_indices+n_minimax]) )
-    i = 0
-    while (alphas_betas_E[-1]/E_old < 1-eps_diff or alphas_betas_E[-1] > E_old):
-
-        E_old = alphas_betas_E[-1]
-        extrema_x = np.append(xdata[0], xdata[argrelextrema(eta_plotting(xdata,alphas_betas_E[0:np.size(alphas_betas_E)-1]), np.greater)[0]])
-        extrema_x = np.append(extrema_x, xdata[argrelextrema(eta_plotting(xdata,alphas_betas_E[0:np.size(alphas_betas_E)-1]), np.less)[0]])
-        print("number of extrema =", np.size(extrema_x))
-        alphas_betas_E[np.size(alphas_betas_E)-1] = np.average(np.abs(eta_plotting(extrema_x,alphas_betas_E[0:np.size(alphas_betas_E)-1])))
-        i += 1
-        print("iteration =", i, "E =",  alphas_betas_E[-1])
-
-        alphas_betas_E = fsolve(eta_for_alphas_betas_E_update, x0=alphas_betas_E, args=extrema_x)
-
     sort_indices = np.argsort(alphas_betas_E[0:n_minimax])
 
     fig1, (axis1) = pl.subplots(1,1)
     axis1.set_xlim((0.8,R_minimax))
     axis1.semilogx(xdata,eta_plotting(xdata,alphas_betas_E))
-    axis1.semilogx([0.8,R_minimax], [alphas_betas_E[-1],alphas_betas_E[-1]])
-    axis1.semilogx([0.8,R_minimax], [-alphas_betas_E[-1],-alphas_betas_E[-1]])
 
     pl.show()
 
@@ -52,15 +33,6 @@ def eta(x, *params):
 def eta_plotting(x, *params):
     params_1d = np.transpose(params)[:,0]
     return 1/(2*x) - (np.exp(-outer(x,params_1d[0:np.size(params)//2]))).dot(params_1d[np.size(params)//2:(np.size(params)//2)*2])
-
-def eta_for_alphas_betas_E_update(x, *params):
-    params_1d = np.transpose(params)[:,0]
-    size_params = np.size(params_1d)
-    size_x = np.size(x)
-    E = np.empty(size_x)
-    E[0:size_x//2+1] = x[size_params-1]
-    E[size_x//2+1:] = -x[size_params-1]
-    return 1/(2*params_1d) - (np.exp(-outer(params_1d,x[0:np.size(x)//2]))).dot(x[np.size(x)//2:np.size(x)-1]) - E
 
 if __name__ == "__main__":
     main()

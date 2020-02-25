removed factor 2, result still wrong

1_other_ranges/minimax_time.py
@@ -117,8 +117,8 @@ def my_fsolve(extrema_x, alphas_betas_E):
     print("")
 
     for index_i in range(n_minimax):
-        mat_J[:,index_i] = -2*extrema_x[0:size_problem]*alphas_betas_E[index_i+n_minimax]*np.exp(-2*extrema_x[0:size_problem]*alphas_betas_E[index_i])
-        mat_J[:,index_i+n_minimax] = np.exp(-2*extrema_x[0:size_problem]*alphas_betas_E[index_i])
+        mat_J[:,index_i] = -extrema_x[0:size_problem]*alphas_betas_E[index_i+n_minimax]*np.exp(-extrema_x[0:size_problem]*alphas_betas_E[index_i])
+        mat_J[:,index_i+n_minimax] = np.exp(-extrema_x[0:size_problem]*alphas_betas_E[index_i])
 
     mat_J[-1,0:n_minimax+1] = alphas_betas_E[-1]
     mat_J[-1,n_minimax+1:2*n_minimax+1] = -alphas_betas_E[-1]
@@ -133,7 +133,7 @@ def my_fsolve(extrema_x, alphas_betas_E):
     delta = gauss(mat_J)
 
     print("")
-    print("delta mysolve =", -delta)
+    print("-delta mysolve =", delta)
     print("")
 
     return alphas_betas_E - delta